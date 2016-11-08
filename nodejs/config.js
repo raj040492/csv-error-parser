3c7ad4970f09bbf851cbb87782058c3f0f6f014c
raj kumar <raj040492@gmail.com>
2016-11-08 17:33:14 +0530

    special characters added to config to help with testing

--- nodejs/config.js
+++ nodejs/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-	threshold_percentage : 60, // above this numerical, positive repsonse else negative		
+	threshold_percentage : 60, // above this numerical, positive repsonse else negative	
+	special_characters : ['`','\\','~','!','@','#','$','_','%','^','&','*','(',')','+','=','[','{',']','}',','<',','.','>','?','/','"','"',';'],
 	states : ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming","District of Columbia","Puerto Rico","Guam","American Samoa","U.S. Virgin Islands","Northern Mariana Islands"],
 	state_codes : ["AK","AL","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"],	
 	states_and_cities : ["Wyoming","Minnesota","California","Georgia","Kansas","Vermont","Indiana","Pennsylvania","Alabama","New York","Florida","Ohio","Texas","Maryland","Louisiana","Missouri","WY","MN","CA","GA","KS","VT","IN","PA","AL","NY","FL","OH","TX","MD","LA"]
