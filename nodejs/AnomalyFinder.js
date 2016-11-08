1b5b7638c2e338e74372498500e2516cc02db7b4
raj kumar <raj040492@gmail.com>
2016-11-08 12:28:39 +0530

    Made major changes, will be explained in the next commit, this commit is just to preserve the commented lines which will be removed in the next commit

--- nodejs/AnomalyFinder.js
+++ nodejs/AnomalyFinder.js
@@ -1,6 +1,7 @@
 // use maps.googleapis.com to find validity of a zipcode// TODO...
 // for ex. http://maps.googleapis.com/maps/api/geocode/json?address=99571 gives details of Alaska ..
 // Improvise from the json
+// TODO.. find ways to reset detail object.. must for full file scan.
 
 var fs = require('fs'),
 	parse = require('csv-parse'),
@@ -13,10 +14,26 @@ var fs = require('fs'),
 	Regex = require("regex"),
 	targetColumnArray = [],
 	possible_dataType = [],
+	possible_anamoly = [],
+	previous_column = '',
+	current_column = '',
 	all_dataType = [],
 	anamolous_datatype = [],
 	rowCount = 0,
-	details = {
+	testCount = 0, //TODO.. remove this later..
+	details = getDetailsObject(),
+	indexNumber = -10; // random number is assigned;
+
+rl.question('Enter the file that you wish to scan > ', function(fileName) {
+    rl.question('Enter the column that you wish to scan > ', function(columnName) {
+	    read_file(fileName,columnName)
+        rl.close();
+    });
+});
+
+function getDetailsObject(){
+
+	return {		
 		"uppercase_entries" : {},
 		"string" : {},
 		"integer" : {},
@@ -27,22 +44,14 @@ var fs = require('fs'),
 		"states" : {},
 		"special_characters" : {},
 		"zip_code" : {}
-	},
-	indexNumber = -10; // random number is assigned;
-
-rl.question('Enter the file that you wish to scan > ', function(fileName) {
-    rl.question('Enter the column that you wish to scan > ', function(columnName) {
-	    read_file(fileName,columnName)
-        rl.close();
-    });
-});
-
+	}	
+}
 
 function read_file (fileName,columnName){
 	fs.createReadStream(fileName)
 		.on('error',function(err){
 			// various errors could be thrown; only if error code is "ENOENT", 
-			// do we conclude that it's a missing file or directory scenario..
+			// do we conclude that, it's a missing file or directory scenario..
 			if (err["code"] == "ENOENT") {
 				console.log("File "+ fileName + " doesnt exist")
 				console.log("Ensure if the file is in the same directory")
@@ -56,155 +65,221 @@ function read_file (fileName,columnName){
 		.pipe(parse({
 				delimiter : ','
 			}))		
-		.on('data',function(rows){	// reads csv row wise			
-			rowCount +=1;
-			if(rowCount == 1) { // the first row of any csv contains heading for each column
-				header = rows				
-			}
-			if(rows.indexOf(columnName)>=0) {
-				columnCheck +=1;				
-				indexNumber = rows.indexOf(columnName);								
-			}
-			targetColumnArray.push(rows[indexNumber]);
-					
+		.on('data',function(rows){	// reads csv, row wise
+
+			create_column(rows,columnName);
 		})
-		.on('end',function(){
-			if(columnCheck > 0 ) { // if the chosen column exists in the csv
-				targetColumnArray.map(function(targetEntry,iterationCount,array){			
-					if(targetEntry != columnName) { // we dont want to classify the header line ; i.e the first line of csv file.
-						classify(targetEntry);
-					}
-					if(iterationCount == array.length-1) {
-						// console.log("details is ",details)
-						analyse(details,array.length-1,columnName)
-					}
-				})
-			}
-			else {
-				console.log("Column " + columnName + " is not present in " + fileName);
-				process.exit(0);
-			}
+		.on('end',function(){			
+			start_process(columnName,fileName)
 		})
 }
 
-function classify(param) {
+function create_column(rows,columnName) {	
+	rowCount +=1;
+	if(rowCount == 1) { // the first row of any csv contains heading for each column
+		header = rows;
+		if (columnName == "ALL") {
+			// array inside array for each columns 
+			// for single column scan, a simple array would do, no need to create child arrays
+			// such new array initialization must happen only once  N only when ALL columns are to scanned
+			// else will break our logic
+			for(var i=0;i<rows.length;i++) {
+				targetColumnArray[i] = new Array();
+			}
+		}
+	}
+	if(rows.indexOf(columnName)>=0) {
+		columnCheck +=1;				
+		indexNumber = rows.indexOf(columnName);								
+	}
+	else if (columnName == "ALL") {
+		columnCheck +=1;
+		for(var i=0;i<rows.length;i++) {
+			targetColumnArray[i].push(rows[i])
+		}
+	}
+	if(columnName != "ALL") {		
+		targetColumnArray.push(rows[indexNumber]);
+	}
+}
+
+function analyse_column(targetEntry,columnName) {
+	targetEntry.map(function(actualEntry,innerArrayIterationCount,innerArray){
+		if(innerArrayIterationCount == 0) {
+			columnName = targetEntry[0]
+		}
+		if(innerArrayIterationCount != 0) { // we dont want to classify the header line ; i.e the first line of csv file.
+			classify(actualEntry,columnName,targetEntry);			
+		}
+		if(innerArrayIterationCount == innerArray.length-1) {
+			analyse(details,innerArray.length-1,columnName)
+		}
+	})
+}
+
+function start_process(columnName,fileName) {
+
+	if(columnCheck > 0 ) { // if the chosen column exists in the csv	
+		targetColumnArray.map(function(targetEntry,iterationCount,array){
+			if(targetEntry.constructor === Array) {
+				analyse_column(targetEntry,columnName)
+			}
+			else {
+				if(targetEntry != columnName) { // we dont want to classify the header line ; i.e the first line of csv file.
+					classify(targetEntry,columnName);
+				}
+				if(iterationCount == array.length-1) {
+					analyse(details,array.length-1,columnName)
+				}
+			}
+		})
+	}
+	else {
+		console.log("Column " + columnName + " is not present in " + fileName);
+		process.exit(0);
+	}
+}
+
+function classify(param,columnName,arrayFullScan) {
 	// a broad wing is created in the first level in which the following will be invoked
 	// if one of the following functions is satisfied by the param, then we go a layer deep 
 	// i.e if param satisfies string, we then check for uppercase and pure string scenario
 	// in other words it doesnt make much sense to put a param thru string and again thru uppercase when
 	// its clear from the first test(string) that it's not a string
-	string(param)
-	integer(param)
-	email(param)
-	special_characters(param)
+
+	if(columnName != current_column) {
+		current_column = columnName
+		details = getDetailsObject();
+	}
+
+	string(details,param,columnName,arrayFullScan)
+	integer(details,param,columnName,arrayFullScan)
+	email(details,param,columnName,arrayFullScan)
+	special_characters(details,param,columnName,arrayFullScan)
 }
 
-function analyse(details,len,columnName) {
+function analyse(details,len,columnName) {		
+	testCount +=1
+	possible_dataType = [];
+	all_dataType = [];
+	possible_anamoly = [];
 	Object.keys(details).map(function(key,iterationCount,array){	
 		if(Object.keys(details[key]).length > 0) {
 			// all_dataType array contains all possible datatypes in said column, even if they occur just once	
-			all_dataType.push(key);
+			if(all_dataType.indexOf(key) == -1) {
+				all_dataType.push(key);
+			}
 		}
 		threshold_percentage = (Object.keys(details[key]).length/len)*100;
+
+		
 		// if a column contains entry is dominated by integers, then in the details object, integer and
 		// associated objects will contains many key-value pairs
 		// when said object's length exceeds a certain threshold (as mentioned in config.js)
 		// we analyse it as belonging to that particular datatype
-		if(threshold_percentage >= config.threshold_percentage) {
+		if(threshold_percentage >= config.threshold_percentage && possible_dataType.indexOf(key) == -1) {
 			possible_dataType.push(key);			
 		}
+		else if(threshold_percentage < config.threshold_percentage && possible_anamoly.indexOf(key) == -1) {
+			possible_anamoly.push(key);			
+		}
 		if(iterationCount == array.length-1) {			
-			possible_dataType.map(function(dataType,count,arr){
+			// possible_dataType.map(function(dataType,count,arr){
 				//possible_dataType contains datatype that can also be anamolous entries
 				// we iteration said array against the never_together object defined in the object
 				// never_together object contains datatypes that can never co-exist together				
-				if(config.never_together[dataType]) {
+				// if(config.never_together[dataType]) {
 					// not all elements in possible_dataType have been defined in never_together object
 					// example string and integer have been commented out.
-					anamolous_datatype = all_dataType.intersection(config.never_together[dataType]);
-				}
-				if(anamolous_datatype.length >0) {
-					console.log("Column "+columnName+ " is dominated by datatypes : "+possible_dataType)
-					anamolous_datatype.map(function(datatype){
+					// anamolous_datatype = all_dataType.intersection(config.never_together[dataType]);
+				// }
+				// if(columnName == "ip_address") {
+				// 	console.log("details is ",details)
+				// }
+				if(possible_anamoly.length >0) {
+					possible_anamoly.map(function(datatype){
+
 						Object.keys(details[datatype]).map(function(key){
-							console.log("Entry " + key + " at row number " + details[datatype][key]["rowCount"] + " is out of place; because it contains datatype " + datatype + "" );
+							console.log("Entry " + key + " at row number " + details[datatype][key]["rowCount"] + " in column "+ columnName +" is out of place; because it contains datatype " + datatype + "" );
 						});
 					});
-					process.exit(0);
+					// details = getDetailsObject()
+					// process.exit(0);
 				}
 				else {
 					if(count == arr.length-1) {
 						// to avoid consoling multiple times..
 						console.log("Column " + columnName + " appears to be clean")	
-						process.exit(0);
+						// process.exit(0);
 					}					
 				}
-			});			
+			// });			
 		}
 	})
 }
 
-function special_characters(param) {
+function special_characters(details,param,columnName,arrayFullScan) {
 	var re = new RegExp(/\`|\~|\!|\@|\#|\$|\_|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\'|\<|\,|\.|\>|\?|\/|\""|\;|\:/g)
 	if(re.test(param)) {
-		construct_detail_object("special_characters",param);
+		construct_detail_object(details,"special_characters",param,columnName,arrayFullScan);
 	}
 }
 
-function uppercase_entries(param) {	
+function uppercase_entries(details,param,columnName,arrayFullScan) {	
 	var re = param.match("[A-Z]");
 	if(re != null) {
-		construct_detail_object("uppercase_entries",param);
+		construct_detail_object(details,"uppercase_entries",param,columnName,arrayFullScan);
 	}
 }
 
-function string(param) {
+function string(details,param,columnName,arrayFullScan) {
+
 	var re = param.match("[a-zA-Z]"); // looks for integer but even mix of string and somethingElse is accepted	
 	if(re != null) {
-		pure_string(param)
-		uppercase_entries(param)
-		construct_detail_object("string",param);		
+		pure_string(details,param,columnName,arrayFullScan)
+		uppercase_entries(details,param,columnName,arrayFullScan)
+		construct_detail_object(details,"string",param,columnName,arrayFullScan);		
 	}
 }
 
-function pure_string(param) {
+function pure_string(details,param,columnName,arrayFullScan) {
 	var re = new RegExp(/^[a-zA-Z]+$/); // looks for exclusively string
-	states(param)
-	state_code(param)
+	states(details,param,columnName,arrayFullScan)
+	state_code(details,param,columnName,arrayFullScan)
 	if(re.test(param)) {			
-		construct_detail_object("pure_string",param);		
+		construct_detail_object(details,"pure_string",param,columnName,arrayFullScan);		
 	}
 }
 
-function states(param) {
+function states(details,param,columnName,arrayFullScan) {
 	if(config.states.indexOf(param) >= 0) {
-		construct_detail_object("states",param)
+		construct_detail_object(details,"states",param,columnName,arrayFullScan)
 	}
 }
 
-function state_code(param) {
+function state_code(details,param,columnName,arrayFullScan) {
 	if(config.state_codes.indexOf(param) >= 0) {
-		construct_detail_object("state_codes",param)
+		construct_detail_object(details,"state_codes",param,columnName,arrayFullScan)
 	}
 }
 
-function integer(param) {
+function integer(details,param,columnName,arrayFullScan) {
 	var re = new RegExp(/\d/); // looks for integer but even mix of integer and somethingElse is accepted
 	if(re.test(param)) {
-		pure_integer(param)	
-		construct_detail_object("integer",param);
+		pure_integer(details,param,columnName,arrayFullScan)	
+		construct_detail_object(details,"integer",param,columnName,arrayFullScan);
 	}
 }
 
-function pure_integer(param) {
+function pure_integer(details,param,columnName,arrayFullScan) {
 	var re = new RegExp(/^\d+$/); // looks for exclusively integer
 	if(re.test(param)) {
 		// zipcode(param)			
-		construct_detail_object("pure_integer",param);
+		construct_detail_object(details,"pure_integer",param,columnName,arrayFullScan);
 	}
 }
 
-function zipcode(param) {
+function zipcode(details,param,columnName,arrayFullScan) {
 	// zipcodes follow various practises; i.e five digits , four digits, five digits and four digits separated by single 
 	//	hyphen five digits and four digits separated by two hyphen
 	// but here we restrict ourselves to just five digits scenario since it is widely used practise
@@ -216,26 +291,29 @@ function zipcode(param) {
 		res.on('end',function(){
 			var data = JSON.parse(body.toString())
 			if(data["status"] == "OK") {
-				construct_detail_object("zip_code",param); //TODO.. OBJECT WONT CONSTRUCT..
+				construct_detail_object(details,"zip_code",param,columnName,arrayFullScan); //TODO.. OBJECT WONT CONSTRUCT..
 			}
 		})
 	})
 }
 
-function email(param) {	
+function email(details,param,columnName,arrayFullScan) {	
 	var re = new RegExp(/\S+@\S+\.\S+/)
 	if(re.test(param)) {			
-		construct_detail_object("email",param);
+		construct_detail_object(details,"email",param,columnName,arrayFullScan);
 	}
 }
 
-function construct_detail_object(datatype,param) {
+function construct_detail_object(details,datatype,param,columnName,arrayFullScan) {
 	// this function updates the details Object with all requisite informations.
 	// this function has been built flexibly so that it can be invoked from multiple functions
-	// console.log("datatype is ",datatype,param)
+
+	rowCount = arrayFullScan == undefined ? targetColumnArray.indexOf(param) : arrayFullScan.indexOf(param)
+
 	details[datatype][param] = {
 		"parameter" : param,
-		"rowCount" : targetColumnArray.indexOf(param)
+		"columnName" : columnName,
+		"rowCount" : rowCount
 	}
 }
 
