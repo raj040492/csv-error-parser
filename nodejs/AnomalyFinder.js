8dbe06b8c51d34cc63b20ea5a099d16fe4028b28
raj kumar <raj040492@gmail.com>
2016-11-14 17:27:08 +0530

    zipcode function removed; till now performance details stored data only for full file scan, now it will also store data for column scan, in performance.txt additional details will be stored; new func store_performance_details_trigger to streamline store performance details under varying cases; small cleanup in individual column scenario (column with duplicate entries case..); few minors changes to increase execution speed;some global variables (like fileSize) were passed via functions, removed now; possible_anamoly was mapped twice, changed to once; remove_duplicate brought into play, till now it was redundant; redundant global variables removed; redundant functions (like array_repetion) removed; unused modules (http, regex) removed;

--- nodejs/AnomalyFinder.js
+++ nodejs/AnomalyFinder.js
@@ -1,23 +1,20 @@
 // TODO unistall pubnub-rickshaw-memory if not needed
+// TODO look into pure_string and whether its needed
+
 var fs = require('fs'),
 	parse = require('csv-parse'),
 	config = require("./config.js"),
-	http = require("http"),	
 	// pnrickmem = require('pubnub-rickshaw-memory'),
 	threshold_percentage = 0,
-	// columnCheck = 0, // to check if the chosen column actually exists in the csv file..
-	Regex = require("regex"),
 	header = '',
 	targetColumnArray = [],
 	fileSize,
 	possible_anamoly = [],
-	previous_column = '',
 	current_column = '',
-	all_dataType = [],
-	anamolous_datatype = [],
-	counts = {},
+	allColumnScan = false, // this variable is used to decide when to trigger storing of performance details
 	rowCountArray = [],
 	rowCount = 0,
+	total_rows = 0,
 	details = getDetailsObject(),
 	indexNumber,
 	start = Date.now(),
@@ -31,6 +28,7 @@ if(fileName && columnName) {
 }
 else {
 	if(fileName && columnName == undefined) {
+		allColumnScan = true;
 		read_file(fileName,"ALL");
 	}
 	else if(fileName == undefined && columnName == undefined) {
@@ -53,22 +51,17 @@ function getDetailsObject(){
 	}	
 }
 function fileSizeInMegabytes(filename) {
-	var stats = fs.statSync(filename)
-	var fileSizeInBytes = stats["size"]
-	//Convert the file size to megabytes 
- 	var fileSizeInMegabytes = fileSizeInBytes / 1000000.0
+	var stats = fs.statSync(filename),
+		fileSizeInBytes = stats["size"],
+ 		fileSizeInMegabytes = fileSizeInBytes / 1000000.0 ; //Convert the file size to megabytes 
 	return fileSizeInMegabytes
 }
 function read_file (fileName,columnName){
 	fileSize = fileSizeInMegabytes(fileName);
 	fs.createReadStream(fileName)
 		.on('error',function(err){
-			// various errors could be thrown; only if error code is "ENOENT", 
-			// do we conclude that, it's a missing file or directory scenario..
-			if (err["code"] == "ENOENT") {
-				console.log("File "+ fileName + " doesnt exist")
-				console.log("Ensure if the file is in the same directory")
-				console.log("Look for typo errors")	
+			if (err["code"] == "ENOENT") { // various errors could be thrown; only if error code is "ENOENT", do we conclude that, it's a missing file or directory scenario..
+				console.log("File "+ fileName + " doesnt exist \nEnsure if the file is in the same directory \nLook for typo errors")	
 				process.exit(0);
 			}
 			else {
@@ -79,9 +72,10 @@ function read_file (fileName,columnName){
 				delimiter : ','
 			}))		
 		.on('data',function(rows){	// reads csv, row wise
+			total_rows +=1;
 			create_column(rows,columnName);
 		})
-		.on('end',function(){			
+		.on('end',function(){
 			start_process(columnName,fileName)
 		})
 }
@@ -89,90 +83,83 @@ function create_column(rows,columnName) {
 	// given header name this function creates an array that contains all entries under that header
 	// ex: if columnName is "id" all entries in csv file under id (1,2,3,4,....100) will be pushed inside an array
 	// said array will scanned and tested subsequently..
-	// if incorrect columnName is fed, the process terminates...
-	// global.targetColumnArray = [];	// here explicit global declaration to aid in testing
-	// global.indexNumber
+	// if incorrect columnName is fed, the process terminates...	
 	rowCount +=1;
 	if(rowCount == 1) { // the first row of any csv contains heading for each column
 		header = rows;
 		if (columnName == "ALL") {
 			// array inside array for each columns 
 			// for single column scan, a simple array would do, no need to create child arrays
-			// such new array initialization must happen only once  N only when ALL columns are to scanned
-			// else will break our logic
-			for(var i=0;i<rows.length;i++) {
+			// such new array initialization must happen only once  N only when ALL columns are to scanned			
+			var len = rows.length // to improve execution speed;
+			for(var i=0;i<len;i++) {
 				targetColumnArray[i] = new Array();
 			}
 		}
 	}
-	if(rows.indexOf(columnName)>=0) {
-		// columnCheck +=1;				
+	if(rows.indexOf(columnName)>=0) {			
 		indexNumber = rows.indexOf(columnName);								
 	}
 	if (columnName == "ALL") {
-		// columnCheck +=1;
-		for(var i=0;i<rows.length;i++) {
+		var len = rows.length // to improve execution speed;
+		for(var i=0;i<len;i++) {
 			targetColumnArray[i].push(rows[i])
 		}
 	}
 	if(columnName != "ALL") {
 		if(indexNumber == undefined){ // indexNumber remains undefined only if the columnName chosen doesnt exist in 
-			// the first line of the csv file.
-			return "column_doesnt_exist"
+			return "column_doesnt_exist" // the first line of the csv file.
 			process.exit(0)
 		}		
-		else {
-			targetColumnArray.push(rows[indexNumber]);
+		else {			
+			if(targetColumnArray.indexOf(rows[indexNumber]) == -1) {
+				targetColumnArray.push(rows[indexNumber]);	
+			}			
 			return "column exists"
 		}		
 	}
 }
-function analyse_column(targetEntry,columnName) {
-	var cleaned_column = targetEntry.filter(function(item, pos) {
-	    return targetEntry.indexOf(item) == pos;
-	})
-	cleaned_column.map(function(actualEntry,innerArrayIterationCount,innerArray){
+function analyse_column(targetEntry,columnName) {	
+	var cleaned_column_length = targetEntry.length; // to improve execution speed;
+	targetEntry.map(function(actualEntry,innerArrayIterationCount,innerArray){
 		if(innerArrayIterationCount == 0) {
 			columnName = targetEntry[0]
 		}
 		if(innerArrayIterationCount != 0) { // we dont want to classify the header line ; i.e the first line of csv file.
 			classify(actualEntry,columnName,targetEntry);			
 		}
-		if(innerArrayIterationCount == innerArray.length-1) {
-			analyse(details,innerArray.length-1,columnName)
+		if(innerArrayIterationCount == cleaned_column_length-1) {
+			analyse(details,cleaned_column_length-1,columnName,targetEntry)
 		}
 	})
 }
 function start_process(columnName,fileName) {
+	var targetColumnArray_length = targetColumnArray.length
 	targetColumnArray.map(function(targetEntry,iterationCount,array){
-		if(targetEntry.constructor === Array) {
-			analyse_column(targetEntry,columnName)
-		}
+		if(targetEntry.constructor === Array) { // when ALL columns are scanned this condition will satisfy because			
+			analyse_column(remove_duplicate(targetEntry),columnName) // in this case, targetColumnArray is an array of 
+		} // arrays, else it's an array of entries...
 		else {
 			if(targetEntry != columnName) { // we dont want to classify the header line ; i.e the first line of csv file.
 				classify(targetEntry,columnName);
 			}
-			if(iterationCount == array.length-1) {
-				analyse(details,array.length-1,columnName)
+			if(iterationCount == targetColumnArray_length-1) {
+				analyse(details,targetColumnArray_length-1,columnName)
 			}
 		}
 	})	
 }
-
 function classify(param,columnName,arrayFullScan) {
 	// a broad wing is created in the first level in which the following will be invoked
-	// if one of the following functions is satisfied by the param, then we go a layer deep 
-	// i.e if param satisfies string, we then check for uppercase and pure string scenario
-	// in other words it doesnt make much sense to put a param thru string and again thru uppercase when
-	// its clear from the first test(string) that it's not a string
+	// if one of the following functions is satisfied by the param, then we go a layer deep i.e if param satisfies string, 
+	// we then check for uppercase and pure string scenario in other words it doesnt make much sense to put a param thru 
+	// string and again thru uppercase when its clear from the first test(string) that it's not a string
 	if(columnName != current_column) {
-		// while scanning the whole file, the flows goes from one column to another
-		// thus when a switch happens from one column to another, we must reset the details object
-		// else the object will contain data from previous column..
+		// while scanning the whole file, the flows goes from one column to another; thus when a switch happens from one 
+		// column to another, we must reset the details object else the object will contain data from previous column.. 
 		// reset happens in getDetailsObject();
 		current_column = columnName
 		rowCountArray = []; //after each column switch
-		counts = {};
 		details = getDetailsObject();
 	}
 	string(details,param,columnName,arrayFullScan)
@@ -182,36 +169,28 @@ function classify(param,columnName,arrayFullScan) {
 	return "classified"
 }
 function analyse(details,len,columnName) {
-	all_dataType = [];
 	not_anamoly = [];
-	possible_anamoly = [];	
+	possible_anamoly = [];
+	var details_key_length = Object.keys(details).length;
 	Object.keys(details).map(function(key,iterationCount,array){
 		threshold_percentage = (Object.keys(details[key]).length/len)*100;
-		// after scanning each column, we have an object (details) that comprises of multiple datatypes
- 		// each datatype has a percentange of occurence (can vary from 0-100%)
-		// if this percentage is lesser than the threshold_percentage (decided in config.js)
-		// then we decide that particular datatype and associated entries to be anamolous.
+		// after scanning each column, we have an object (details) that comprises of multiple datatypes; each datatype has a 
+		// percentange of occurence (can vary from 0-100%) ;if this percentage is lesser than the threshold_percentage 
+		//(decided in config.js) then we decide that particular datatype and associated entries to be anamolous.
 		if(threshold_percentage < config.threshold_percentage && threshold_percentage >0 && possible_anamoly.indexOf(key) == -1) {
 			possible_anamoly.push(key);
-			console.log("threshold_percentage for "+key+" in "+ columnName+" is"+threshold_percentage+"")			
 		}
 		else if(threshold_percentage > config.threshold_percentage && not_anamoly.indexOf(key) == -1) {
 			not_anamoly.push(key);
-			console.log("dominant.. threshold_percentage for "+key+" in "+ columnName+" is"+threshold_percentage+"")			
 		}
-		if(iterationCount == array.length-1) {
-			manage_exceptions(possible_anamoly,not_anamoly,columnName);			
+		if(iterationCount == details_key_length-1) {
+			manage_exceptions(not_anamoly,columnName);			
 		}
 	})
 }
-function manage_exceptions(possible_anamoly,not_anamoly,columnName) {
-	console.log("possible_anamoly inside manage_exceptions is ",possible_anamoly)
-	console.log("not_anamoly inside manage_exceptions is ",not_anamoly)
+function manage_exceptions(not_anamoly,columnName) {
 	possible_anamoly.map(function(anamolous_datatype){
-		console.log("config.never_together["+anamolous_datatype+"] is ",config.never_together[anamolous_datatype])
-		// console.log("config.never_together["+anamolous_datatype+"] is ",config.never_together[anamolous_datatype])
 		if(config.never_together[anamolous_datatype] != undefined && common_array_entries(not_anamoly,config.never_together[anamolous_datatype]) == false) {
-			console.log("no common..")
 			not_anamoly.map(function(non_anamolous_datatype){
 				if(config.exceptions[anamolous_datatype] && config.exceptions[anamolous_datatype].indexOf(non_anamolous_datatype)>=0) {
 					var index = possible_anamoly.indexOf(anamolous_datatype)
@@ -227,43 +206,47 @@ function manage_exceptions(possible_anamoly,not_anamoly,columnName) {
 				}
 			});
 		}
-
+		not_anamoly.map(function(non_anamolous_datatype){	
+			if(config.exceptions[anamolous_datatype] && config.exceptions[anamolous_datatype].indexOf(non_anamolous_datatype)>=0) {
+				var index = possible_anamoly.indexOf(anamolous_datatype)
+				possible_anamoly.splice(index,1);
+			}
+		});
 	});
-	console.log("possible_anamoly after manage_exceptions is ",possible_anamoly)
-	// possible_anamoly.map(function(anamolous_datatype){
-	// 	not_anamoly.map(function(non_anamolous_datatype){	
-	// 		if(config.exceptions[anamolous_datatype] && config.exceptions[anamolous_datatype].indexOf(non_anamolous_datatype)>=0) {
-	// 			var index = possible_anamoly.indexOf(anamolous_datatype)
-	// 			possible_anamoly.splice(index,1);
-	// 		}
-	// 	});
-	// })	
-	// show_results(possible_anamoly,not_anamoly,columnName)
-	
+	show_results(columnName)	
 }
-function show_results(possible_anamoly,not_anamoly,columnName){
-	console.log("possible_anamoly inside show_results is ",possible_anamoly)
+function show_results(columnName){
 	if(possible_anamoly.length >0) {
+		var possible_anamoly_length = possible_anamoly.length;
 		possible_anamoly.map(function(datatype,anamolyIterationCount,possibleAnamolyArray){
 			Object.keys(details[datatype]).map(function(key){
 				console.log("\x1b[31m","Entry " + key + " at row number " + details[datatype][key]["rowCount"] + " in column "+ columnName +" is out of place; because it contains datatype " + datatype + "" ,"\x1b[0m");
-			});					
-			if(header.indexOf(columnName) == header.length-1) {
-				end = Date.now();
-				timeTaken = (end-start)/1000;
-				store_performance_details(fileSize, timeTaken)
-				process.exit(0)
+			});	
+			if(anamolyIterationCount == possible_anamoly_length-1){
+				store_performance_details_trigger(timeTaken,allColumnScan,columnName);
 			}
 		});
+		
 	}
 	else {
 		console.log("\x1b[32m","Column " + columnName + " appears to be clean","\x1b[0m");
+		store_performance_details_trigger(timeTaken,allColumnScan,columnName);
 		return "clean_column"
 	}
 }
-function store_performance_details(fileSize,timeTaken) {
+function store_performance_details_trigger(timeTaken,allColumnScan,columnName){
+	end = Date.now();
+	timeTaken = (end-start)/1000;
+	if(allColumnScan == true && header.indexOf(columnName) == header.length-1) { // When all Columns are Scanned the 		
+		store_performance_details(timeTaken,header.length,targetColumnArray) //performance function should be invoked only 
+	} // after the last column is scanned; thus the if loop;
+	else if(allColumnScan == false){ // when single column is scanned no of columns scanned should be registered as "1"
+		store_performance_details(timeTaken,1,targetColumnArray)
+	}
+}
+function store_performance_details(timeTaken,columnsScanned) {
 	var heapUsed = process.memoryUsage()["heapUsed"],
-		performanceDetails = "Time Taken : "+timeTaken+" secs \nFileSize : "+ fileSize+" MB \nMemory Heap Used : "+ heapUsed +"\nTime: "+Date(Date.now())+"\n\n";
+		performanceDetails = "Time Taken : "+timeTaken+" secs \nFileSize : "+ fileSize+" MB \nMemory Heap Used : "+ heapUsed + "\nColumns Scanned : "+columnsScanned+ "\nRows Scanned : "+total_rows+ "\nTime: "+Date(Date.now())+"\n\n";
 	fs.appendFileSync('performance.txt', performanceDetails, 'utf8')
 }
 function special_characters(details,param,columnName,arrayFullScan) {
@@ -341,31 +324,13 @@ function integer(details,param,columnName,arrayFullScan) {
 }
 function pure_integer(details,param,columnName,arrayFullScan) {
 	var re = new RegExp(/^\d+$/); // looks for exclusively integer
-	if(re.test(param)) {
-		// zipcode(param)			
+	if(re.test(param)) {		
 		construct_detail_object(details,"pure_integer",param,columnName,arrayFullScan);
 		return re.test(param)
 	}
 	else {
 		return false
 	}
-}
-function zipcode(details,param,columnName,arrayFullScan) {
-	// zipcodes follow various practises; i.e five digits , four digits, five digits and four digits separated by single 
-	//	hyphen five digits and four digits separated by two hyphen
-	// but here we restrict ourselves to just five digits scenario since it is widely used practise
-	var body = [];
-	http.get("http://maps.googleapis.com/maps/api/geocode/json?address="+param +"",function(res){
-		res.on('data',function(data){
-			body.push(data);
-		});
-		res.on('end',function(){
-			var data = JSON.parse(body.toString())
-			if(data["status"] == "OK") {
-				construct_detail_object(details,"zip_code",param,columnName,arrayFullScan); //TODO.. OBJECT WONT CONSTRUCT..
-			}
-		})
-	})
 }
 function email(details,param,columnName,arrayFullScan) {	
 	var re = new RegExp(/\S+@\S+\.\S+/)
@@ -379,38 +344,27 @@ function email(details,param,columnName,arrayFullScan) {
 }
 function construct_detail_object(details,datatype,param,columnName,arrayFullScan) {
 	// this function updates the details Object with all requisite informations.
-	// this function has been built flexibly so that it can be invoked from multiple functions	
+	// this function has been built flexibly so that it can be invoked from multiple functions	with varying parameters
 	rowCount = arrayFullScan == undefined ? targetColumnArray.indexOf(param) : arrayFullScan.indexOf(param)
 	rowCountArray.push(rowCount)
-	var emptyObj = {};	
-	repetitionCount = arrayFullScan != undefined ? array_repetion(arrayFullScan,emptyObj) : ""
-	
-	//when single column is scanned targetColumnArray will comprise of single array where we use indexOf to find our param
+	//when single column is scanned "targetColumnArray" will comprise of single array where we use indexOf to find our param
 	//when whole file is scanned targetColumnArray comprises of multiple array ;now we pick each child array(arrayFullScan)
 	// and find the indexOf param in it.
 	details[datatype][param] = {
 		"parameter" : param,
 		"columnName" : columnName,
-		"repetitionCount" : repetitionCount,
-		"rowCount" : rowCount
+		"rowCount" : rowCount+1 
 	}
 	return details[datatype]
 }
-function array_repetion(arr,counts){
-	for(var i = 0; i< arr.length; i++) {
-	    var num = arr[i];
-	    counts[num] = counts[num] ? counts[num]+1 : 1;
-	}
-	return counts
-}
 function remove_duplicate(array){
-	array.filter(function(item, pos) {
+	return array.filter(function(item, pos) {
 	    return array.indexOf(item) == pos;
 	})
 }
-var common_array_entries = function (haystack, arr) {
+function common_array_entries(array, arr) {
     return arr.some(function (v) {
-        return haystack.indexOf(v) >= 0;
+        return array.indexOf(v) >= 0;
     });
 }
 
